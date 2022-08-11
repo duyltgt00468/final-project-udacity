d2a53d3ce91d3a5131b8b41f398111cbb35939f7
Luu The Duy <luutheduy2@gmail.com>
2022-08-11 22:01:00 +0700

    dev | Update code logic in lambda function

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,22 +1,45 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
+import { AttachmentUtils } from '../../dataLayer/attachmentUtils'
+import { updateAttachmentUrl } from '../../bussinessLogic/feed'
 import { getUserId } from '../utils'
+import { createLogger } from "../../utils/logger";
+import { v4 as uuidv4 } from 'uuid';
+
+const logger = createLogger("generateUpload-feed");
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+    const feedId = event.pathParameters.feedId
+    const attachmentUtils = new AttachmentUtils()
 
-    return undefined
+    const userId = getUserId(event)
+    try {
+      const attachmentId = uuidv4()
+      let uploadUrl = await attachmentUtils.createAttachmentPresignedUrl(attachmentId);
+      const attachmentUrl = await attachmentUtils.getAttachmentUrl(attachmentId)
+      await updateAttachmentUrl(userId, feedId, attachmentUrl)
+      return {
+        statusCode: 201,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          uploadUrl: uploadUrl
+        })
+      }
+    } catch (e) {
+      logger.error(e.message)
+      return {
+        statusCode: 500,
+        body: e.message
+      }
+    }
   }
 )
-
 handler
   .use(httpErrorHandler())
   .use(
